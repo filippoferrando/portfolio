5fd9a87cc4394a65389a72f61231c6a1e8877ad9
filippoferrando <ferrandodfilippo@gmail.com>
2021-06-03 08:29:36 +0200

    update

---
+++ build/precache-manifest.39159add65b4a9ddd08e2e575dcc93ca.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ad6d0442286399bae2b86429fbc4df5",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "0ff04fccdb95f4e7ce2a",
+    "url": "/portfolio/static/css/main.815c0ef0.chunk.css"
+  },
+  {
+    "revision": "2248ac4b9b80382410b5",
+    "url": "/portfolio/static/js/2.3a538740.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/portfolio/static/js/2.3a538740.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ff04fccdb95f4e7ce2a",
+    "url": "/portfolio/static/js/main.447f181b.chunk.js"
+  },
+  {
+    "revision": "5c1a5850ce36742f41dd",
+    "url": "/portfolio/static/js/runtime-main.737fe1ef.js"
+  },
+  {
+    "revision": "d963a85f82811979a72d091762e82711",
+    "url": "/portfolio/static/media/8ogqpfkvqqpyfbs3w6p7.d963a85f.webp"
+  },
+  {
+    "revision": "f7ab02d6c22d9bbff6697fb0d4b0f1c9",
+    "url": "/portfolio/static/media/Blockchain11.f7ab02d6.jpg"
+  },
+  {
+    "revision": "ee602d986e3d30a22f9908c7fdc7cc0a",
+    "url": "/portfolio/static/media/about.ee602d98.gif"
+  },
+  {
+    "revision": "adc8d679938835097de97fa8066f200e",
+    "url": "/portfolio/static/media/bqc1gncxdhau0zfuxqmp.adc8d679.webp"
+  },
+  {
+    "revision": "eaa5b671ebb04910cd9172885891d484",
+    "url": "/portfolio/static/media/brainfuck.eaa5b671.jpg"
+  },
+  {
+    "revision": "49828655ad188fcd671d95a45289773a",
+    "url": "/portfolio/static/media/default-project.49828655.jpg"
+  },
+  {
+    "revision": "7bd4df04845984c1615fe5d2a6045f39",
+    "url": "/portfolio/static/media/devcamper-project.7bd4df04.jpg"
+  },
+  {
+    "revision": "3897520096c1c014c581cae52d12dea6",
+    "url": "/portfolio/static/media/devconnector-project.38975200.jpg"
+  },
+  {
+    "revision": "4517ae84f6f9d5c378147a3ee4c195b5",
+    "url": "/portfolio/static/media/developer.4517ae84.gif"
+  },
+  {
+    "revision": "b32605eb34ff9b9ef81a10755266a6ca",
+    "url": "/portfolio/static/media/ecommerce-project.b32605eb.jpg"
+  },
+  {
+    "revision": "edf8119ef4c968f250eb2fd02f5c1279",
+    "url": "/portfolio/static/media/github-project.edf8119e.jpg"
+  },
+  {
+    "revision": "2f4f6e395e0a36cfc9ea503592565167",
+    "url": "/portfolio/static/media/header-chat-box.2f4f6e39.png"
+  },
+  {
+    "revision": "d6bb6832f41789ae7ce4c8e8a9c736d9",
+    "url": "/portfolio/static/media/movieapp-project.d6bb6832.jpg"
+  },
+  {
+    "revision": "620b4aa08729cb9d3157d9db5b6da720",
+    "url": "/portfolio/static/media/notfound.620b4aa0.gif"
+  },
+  {
+    "revision": "ab29312c4db1e43f1d594a35c81e4e2d",
+    "url": "/portfolio/static/media/skills.ab29312c.gif"
+  }
+]);
